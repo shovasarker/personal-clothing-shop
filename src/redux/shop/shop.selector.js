2967673ee3b0696123a2011994bf3b3262aa3e59
Shova Sarker <shovasarker01@gmail.com>
2022-01-01 04:05:47 +0600

    Add files via upload

---
+++ src/redux/shop/shop.selector.js
@@ -0,0 +1,21 @@
+import { createSelector } from 'reselect'
+import memoize from 'lodash.memoize'
+
+const selectShop = (state) => state.shop
+
+export const selectShopCollections = createSelector(
+  [selectShop],
+  (shop) => shop.collections
+)
+
+export const selectShopCollectionsForPreview = createSelector(
+  [selectShopCollections],
+  (collections) => Object.keys(collections).map((key) => collections[key])
+)
+
+export const selectShopCollectionItems = memoize((collectionUrlParam) => {
+  return createSelector(
+    [selectShopCollections],
+    (collections) => collections[collectionUrlParam]
+  )
+})
